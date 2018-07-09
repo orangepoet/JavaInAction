44375c737f70c90dfe1bdd02925dedb8abf9cd72
orangepoet <chengzhi37@gmail.com>
2018-07-09 22:16:18 +0800

    parallel module

---
+++ inaction-parallel/src/main/java/cn/orangepoet/inaction/parallel/condition/ConditionDemo.java
@@ -0,0 +1,91 @@
+package cn.orangepoet.inaction.parallel.condition;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 条件队列的使用, Java 监视器锁和等待/通知机制的高级版
+ * 参照 {@link NotifyDemo}, 使用了Condition对象的await()和signalAll()方法协调等待/通知
+ */
+public class ConditionDemo {
+    private static ReentrantLock lock = new ReentrantLock();
+    private static Condition condition = lock.newCondition();
+
+    private static final int WAITER_COUNT = 10;
+    private static CountDownLatch startGate = new CountDownLatch(WAITER_COUNT);
+    private static CountDownLatch endGate = new CountDownLatch(WAITER_COUNT);
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < WAITER_COUNT; i++) {
+            Thread waiterThread = new Thread(new Waiter(), "waiter-" + (i + 1));
+            waiterThread.start();
+        }
+
+        Thread notifierThread = new Thread(new Notifer(), "notifer");
+        notifierThread.start();
+
+        try {
+            endGate.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("all run finish");
+    }
+
+    public static class Waiter implements Runnable {
+        @Override
+        public void run() {
+            startGate.countDown();
+            System.out.println(getThreadName() + ": prepare to run");
+//            synchronized (lock) {
+            lock.lock();
+            try {
+                condition.await();
+                System.out.println(getThreadName() + ": receive notify and go on");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+            System.out.println(getThreadName() + ": finish");
+            endGate.countDown();
+        }
+
+        public String getThreadName() {
+            return Thread.currentThread().getName();
+        }
+    }
+
+    public static class Notifer implements Runnable {
+        @Override
+        public void run() {
+            int i = 0;
+            while (true) {
+                try {
+                    startGate.await();
+//                    synchronized (lock) {
+//                        System.out.println(Thread.currentThread().getName() + ": prepare to notify ");
+//                        lock.notifyAll();
+//                    }
+                    lock.lock();
+                    try {
+                        System.out.println(Thread.currentThread().getName() + ": prepare to notify ");
+                        condition.signalAll();
+                        i++;
+                    } finally {
+                        lock.unlock();
+                    }
+                    if (i >= WAITER_COUNT) {
+                        break;
+                    }
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
---
+++ inaction-parallel/src/main/java/cn/orangepoet/inaction/parallel/condition/NotifyDemo.java
@@ -0,0 +1,75 @@
+package cn.orangepoet.inaction.parallel.condition;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * 对比使用 通知/通知和条件队列, 参照 {@link ConditionDemo}条件队列
+ * 演示线程wait() 和notifyAll(), 等待/通知机制
+ * 使用CountDownLatch控制程序的并发执行, 同步栅栏, 线程调用await()阻塞直到CountDownLatch初始化状态值变量被一次次调用countDown()
+ * 方法减少到0时, 栅栏释放, 等待线程继续执行, 分别使用了开始门和结束门, 控制程序并发执行和等待所有并发执行完成;
+ */
+public class NotifyDemo {
+    private static Object lock = new Object();
+    private static final int WAITER_COUNT = 10;
+    private static CountDownLatch startGate = new CountDownLatch(WAITER_COUNT);
+    private static CountDownLatch endGate = new CountDownLatch(WAITER_COUNT);
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < WAITER_COUNT; i++) {
+            Thread waiterThread = new Thread(new Waiter(), "waiter-" + (i + 1));
+            waiterThread.start();
+        }
+
+        Thread notifierThread = new Thread(new Notifer(), "notifer");
+        notifierThread.start();
+
+        try {
+            endGate.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("all run finish");
+    }
+
+    public static class Waiter implements Runnable {
+        @Override
+        public void run() {
+            startGate.countDown();
+            System.out.println(getThreadName() + ": prepare to run");
+            synchronized (lock) {
+                try {
+                    lock.wait();
+                    System.out.println(getThreadName() + ": receive notify and go on");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println(getThreadName() + ": finish");
+            endGate.countDown();
+        }
+
+        public String getThreadName() {
+            return Thread.currentThread().getName();
+        }
+    }
+
+    public static class Notifer implements Runnable {
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    startGate.await();
+                    synchronized (lock) {
+                        System.out.println(Thread.currentThread().getName() + ": prepare to notify ");
+                        lock.notifyAll();
+                    }
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
