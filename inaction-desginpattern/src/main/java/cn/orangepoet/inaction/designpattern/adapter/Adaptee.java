7e4531392c2b196fc453b6f6997c021e24f386c6
chengzhi.cz <chengzhi.cz@alibaba-inc.com>
2019-10-10 20:43:05 +0800

    adapter

---
+++ inaction-desginpattern/src/main/java/cn/orangepoet/inaction/designpattern/adapter/Adaptee.java
@@ -0,0 +1,11 @@
+package cn.orangepoet.inaction.designpattern.adapter;
+
+/**
+ * @author chengzhi
+ * @date 2019/10/10
+ */
+public class Adaptee {
+    public int request(String str) {
+        return 1;
+    }
+}
---
+++ inaction-desginpattern/src/main/java/cn/orangepoet/inaction/designpattern/adapter/Target.java
@@ -0,0 +1,9 @@
+package cn.orangepoet.inaction.designpattern.adapter;
+
+/**
+ * @author chengzhi
+ * @date 2019/10/10
+ */
+public interface Target {
+    int request(int j);
+}
