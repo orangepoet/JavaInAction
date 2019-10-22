02a1db840c4db4cb9154ba9d2a5baa1de493b9ce
orangepoet <chengzhi37@gmail.com>
2019-10-23 00:05:10 +0800

    upd flight attack

--- inaction/flight_attack.py
+++ inaction/flight_attack.py
@@ -1,132 +1,181 @@
+# coding=utf-8
 
-map = []
+status_empty = 0
+status_body = 1
+status_head = 2
 
-status_unknown=-1
-status_empty=0
-status_body=1
-status_head=2
+x_max = 10
+y_max = 10
 
-x_max=10
-y_max=10
 
 def init_map():
-    for i in range(0,x_max):
-        row = []
-        for j in range(0,y_max):
-            row.append((i,j))
-        
-def print_flight(dots):
-    return 
+    """
+    地图初始化
+    """
+    ret = []
+    for i in range(0, x_max):
+        row = [0 for j in range(0, y_max)]
+        ret.append(row)
+    return ret
+
+
+def print_map(map):
+    """
+    打印最终地图
+    :type map: list
+    """
     print('--------------------')
-    for y in range(0,y_max):
+    for y in range(0, y_max):
         line = ''
-        for x in range(0,x_max):
-            dot = (x,y)
-            if dot in dots:
-                line+=' H'
+        for x in range(0, x_max):
+            dot = map[x][y]
+            if dot == status_body:
+                line += ' B'
+            elif dot == status_head:
+                line += ' H'
             else:
-                line+=' *'
+                line += ' .'
         print(line)
     print('--------------------')
 
-def is_position_in_map(dots):
-    for dot in dots:
-        if dot[0]>=0 and dot[0]<=9 and dot[1]>=0 and dot[1]<=9:
-            return False
-    return True
+
+def print_flight(flight):
+    """
+    打印飞机
+    :param flight: 飞机的点阵
+    """
+    map = init_map()
+    place_flight(flight, map)
+    print_map(map)
+
+
+def is_flight_can_place(flight, map):
+    """
+    尝试放置飞机
+    :type map: list
+    :param flight: 目标飞机
+    :return: True: 成功放下, False: 失败, 不能放下
+    """
+    return all(0 <= x <= 9 and 0 <= y <= 9 and map[x][y] == status_empty for (x, y) in flight)
+
 
 def get_up_flight(head):
     x = head[0]
     y = head[1]
+    ret = [head]
 
-    ret = []
-
-    for i in range(x-2,x+2+1):
-        ret.append((i,y+1))
-    for i in range(x-1,x+1+1):
-        ret.append((i,y+3))
-    for j in range(y+1,y+3+1):
-        ret.append((x,j))
+    for i in range(x - 2, x + 2 + 1):
+        ret.append((i, y + 1))
+    for i in range(x - 1, x + 1 + 1):
+        ret.append((i, y + 3))
+    for j in range(y + 1, y + 3 + 1):
+        ret.append((x, j))
     return ret
+
 
 def get_down_flight(head):
     x = head[0]
     y = head[1]
-    ret = []
+    ret = [head]
 
-    for i in range(x-2,x+2+1):
-        ret.append((i,y-1))
-    for i in range(x-1,x+1+1):
-        ret.append((i,y-3))
-    for j in range(y-3,y-1+1):
-        ret.append((x,j))
+    for i in range(x - 2, x + 2 + 1):
+        ret.append((i, y - 1))
+    for i in range(x - 1, x + 1 + 1):
+        ret.append((i, y - 3))
+    for j in range(y - 3, y - 1 + 1):
+        ret.append((x, j))
     return ret
+
 
 def get_left_flight(head):
     x = head[0]
     y = head[1]
+    ret = [head]
 
-    ret = []
-
-    for j in range(y-2,y+2+1):
-        ret.append((x+1,j))
-    for j in range(y-1,y+1):
-        ret.append(x+3,y-1)
-    for i in range(x+1, x+3+1):
-        ret.append(i,y)
+    for j in range(y - 2, y + 2 + 1):
+        ret.append((x + 1, j))
+    for j in range(y - 1, y + 1):
+        ret.append((x + 3, y - 1))
+    for i in range(x + 1, x + 3 + 1):
+        ret.append((i, y))
     return ret
+
 
 def get_right_flight(head):
     x = head[0]
     y = head[1]
+    ret = [head]
 
-    ret = []
-
-    for j in range(y-2,y+2+1):
-        ret.append((x-1,j))
-    for j in range(y-1,y+1+1):
-        ret.append((x-3,j))
-    for i in range(x-3,x-1+1):
-        ret.append((i,y))
+    for j in range(y - 2, y + 2 + 1):
+        ret.append((x - 1, j))
+    for j in range(y - 1, y + 1 + 1):
+        ret.append((x - 3, j))
+    for i in range(x - 3, x - 1 + 1):
+        ret.append((i, y))
     return ret
 
-def get_flight_cnt(head):
-    count = 0
-    flight0 = get_up_flight(head)
-    if is_position_in_map(flight0):
-        count+=1
 
-    flight1 = get_down_flight(head)
-    if is_position_in_map(flight1):
-        count+=1
+def list_flight_by_head(head, map):
+    """
+    枚举该点上所有的飞机
+    :param map:
+    :param head:
+    :return: 该点上所有的飞机
+    """
+    all_flight = [get_up_flight(head), get_down_flight(head), get_left_flight(head), get_right_flight(head)]
+    return list(filter(lambda f: is_flight_can_place(f, map), all_flight))
 
-    flight2 = get_left_flight(head)
-    if is_position_in_map(flight2):
-        count+=1
-    
-    fligt3 = get_right_flight(head)
-    if is_position_in_map(fligh3):
-        count+=1
-    return count
 
-def get_all_head():
-    valid_head=[]
-    head_cnt=0
-    for y in range(0,y_max):
-        for x in range(0,x_max):
-            head=(x,y)
-            cnt = get_flight_cnt(head)
-            if cnt>0:
-                head_cnt+=1
-                print(str(head)+": "+str(cnt))
-            else:
-                pass
-    # print(str(valid_head))
-    print('total cnt:'+str(head_cnt))
+def list_all_flight(map):
+    """
+    枚举所有可能的飞机
+    :type map: list
+    :return: 所有飞机
+    """
+    ret = []
+    for y in range(0, y_max):
+        for x in range(0, x_max):
+            flights = list_flight_by_head((x, y), map)
+            if flights:
+                for f in flights:
+                    ret.append(f)
+    return ret
+
+
+def place_flight(flight, map):
+    """
+    放置飞机
+    :param flight:
+    :return:
+    """
+    head = flight[0]
+    map[head[0]][head[1]] = status_head
+
+    body = flight[1:]
+    for dot in body:
+        map[dot[0]][dot[1]] = status_body
+
+
+def place_flights():
+    """
+    在棋盘上放置飞机
+    :param num: 飞机数量
+    """
+    map = init_map()
+    for first in list_all_flight(map):
+        place_flight(first, map)
+        for second in list_all_flight(map):
+            place_flight(second, map)
+            for third in list_all_flight(map):
+                place_flight(third, map)
+                print_map(map)
+                map = init_map()
 
 
 def main():
-    get_all_head()
+    init_map()
+    place_flights()
+
 
 if __name__ == "__main__":
     main()
