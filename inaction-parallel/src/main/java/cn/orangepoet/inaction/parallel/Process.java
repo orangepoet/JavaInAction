e56ac224ecd28d6a7d32106d6c13749686132122
chengzhi.cz <chengzhi.cz@alibaba-inc.com>
2019-12-11 10:27:08 +0800

    #spring: config configurationProperties

--- inaction-parallel/src/main/java/cn/orangepoet/inaction/parallel/Process.java
+++
@@ -1,9 +0,0 @@
-package cn.orangepoet.inaction.parallel;
-
-import cn.orangepoet.inaction.parallel.example.SchedulerTask;
-
-public class Process {
-    public static void main(String[] args) {
-        new SchedulerTask().execute();
-    }
-}
