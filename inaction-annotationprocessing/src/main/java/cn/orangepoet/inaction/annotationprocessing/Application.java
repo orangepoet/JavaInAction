3392f335ee0be52b2a26d0d3b89ee83287453ebc
chengzhi.cz <chengzhi.cz@alibaba-inc.com>
2020-01-17 10:30:46 +0800

    + annotation processing

---
+++ inaction-annotationprocessing/src/main/java/cn/orangepoet/inaction/annotationprocessing/Application.java
@@ -0,0 +1,15 @@
+package cn.orangepoet.inaction.annotationprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chengzhi
+ * @date 2020/01/17
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
