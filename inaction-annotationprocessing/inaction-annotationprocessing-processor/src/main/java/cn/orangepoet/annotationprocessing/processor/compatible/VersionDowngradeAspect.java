88f481afd5a6134aa82c4c012057f0179f3d338f
orangepoet <chengzhi37@gmail.com>
2020-03-15 21:44:13 +0800

    clean import

--- inaction-annotationprocessing/inaction-annotationprocessing-processor/src/main/java/cn/orangepoet/annotationprocessing/processor/compatible/VersionDowngradeAspect.java
+++ inaction-annotationprocessing/inaction-annotationprocessing-processor/src/main/java/cn/orangepoet/annotationprocessing/processor/compatible/VersionDowngradeAspect.java
@@ -1,6 +1,5 @@
 package cn.orangepoet.annotationprocessing.processor.compatible;
 
-import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
@@ -9,7 +8,6 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
-import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 import java.util.List;
