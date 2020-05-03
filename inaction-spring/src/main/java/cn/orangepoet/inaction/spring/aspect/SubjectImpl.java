08b56a550aee673a38e23384137a510a1d25d006
chengzhi.cz <chengzhi.cz@alibaba-inc.com>
2020-05-03 09:14:45 +0800

    exposeProxy #aop

--- inaction-spring/src/main/java/cn/orangepoet/inaction/spring/aspect/SubjectImpl.java
+++ inaction-spring/src/main/java/cn/orangepoet/inaction/spring/aspect/SubjectImpl.java
@@ -1,12 +1,12 @@
 package cn.orangepoet.inaction.spring.aspect;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.google.common.base.Preconditions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author orangecheng
---
+++ inaction-spring/src/main/java/cn/orangepoet/inaction/spring/aspect/proxy/ProxySelfMethod.java
@@ -0,0 +1,23 @@
+package cn.orangepoet.inaction.spring.aspect.proxy;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.aop.framework.AopContext;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author chengzhi
+ * @date 2020/05/03
+ */
+@Component
+@Slf4j
+public class ProxySelfMethod {
+    public void method1() {
+        log.info("method1");
+        ((ProxySelfMethod)AopContext.currentProxy()).method2();
+        //method2();
+    }
+
+    public void method2() {
+        log.info("method2");
+    }
+}
