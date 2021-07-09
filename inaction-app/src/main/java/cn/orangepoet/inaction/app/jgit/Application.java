49f79341af3e17564a16a9daa7e52e03f86aac68
chengzhi.cz <chengzhi.cz@alibaba-inc.com>
2021-07-09 17:31:35 +0800

    + jgit

---
+++ inaction-app/src/main/java/cn/orangepoet/inaction/app/jgit/Application.java
@@ -0,0 +1,80 @@
+package cn.orangepoet.inaction.app.jgit;
+
+import org.apache.commons.lang3.StringUtils;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+import org.eclipse.jgit.transport.CredentialsProvider;
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author chengzhi
+ * @date 2021/07/09
+ */
+public class Application {
+    private static final String REMOTE_URL = "https://github.com/orangepoet/JavaInAction.git";
+    private static final String LOCAL_URL = "/Users/orangecheng/codes/JavaInAction/inaction";
+
+    public static void main(String[] args) {
+        listBranches();
+
+        createBranch();
+
+        checkBranch();
+    }
+
+    private static void listBranches() {
+        try {
+            Map<String, Ref> remoteRefs = Git.lsRemoteRepository()
+                .setHeads(true)
+                .setTags(true)
+                .setRemote(REMOTE_URL)
+                .callAsMap();
+
+            System.out.println("--------------------------------------------");
+            remoteRefs.forEach((k, v) -> {
+                System.out.println(StringUtils.replace(k, "refs/heads/", ""));
+            });
+        } catch (GitAPIException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void createBranch() {
+        try (Repository repository = getLocalRepository()) {
+            try (Git git = new Git(repository)) {
+                git.branchCreate().setName("testBranch").call();
+            }
+        } catch (IOException | GitAPIException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void checkBranch() {
+        try (Repository repository = getLocalRepository()) {
+            try (Git git = new Git(repository)) {
+                git.checkout().setName("testBranch").call();
+
+                CredentialsProvider credentialsProvider = new UsernamePasswordCredentialsProvider("orangepoet", "Cheng5210");
+                git.pull().setCredentialsProvider(credentialsProvider).call();
+
+                List<Ref> refs = git.branchList().call();
+                for (Ref ref : refs) {
+                    System.out.println(ref.getName());
+                }
+            }
+        } catch (IOException | GitAPIException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static Repository getLocalRepository() throws IOException {
+        return new FileRepositoryBuilder().readEnvironment().findGitDir().build();
+    }
+}
