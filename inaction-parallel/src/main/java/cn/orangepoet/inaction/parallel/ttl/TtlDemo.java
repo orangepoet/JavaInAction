208942531ae9d084bc638dd4188edbc73924fe14
chengzhi.cz <chengzhi.cz@alibaba-inc.com>
2022-03-11 17:16:49 +0800

    clean code

--- inaction-parallel/src/main/java/cn/orangepoet/inaction/parallel/ttl/TtlDemo.java
+++ inaction-parallel/src/main/java/cn/orangepoet/inaction/parallel/ttl/TtlDemo.java
@@ -4,8 +4,6 @@ import com.alibaba.ttl.TransmittableThreadLocal;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ForkJoinPool;
-import java.util.stream.IntStream;
 
 /**
  * @author chengzhi
@@ -13,47 +11,7 @@ import java.util.stream.IntStream;
  */
 @Slf4j
 public class TtlDemo {
-    private static ForkJoinPool forkJoinPool = new ForkJoinPool(16);
-
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        log.info("commonPool.size: {}", ForkJoinPool.commonPool().getPoolSize());
-
-        log.info("pool size: {}", forkJoinPool.getPoolSize());
-
-        forkJoinPool.submit(() -> {
-            IntStream.range(1, 100).parallel().forEach(i -> {
-                try {
-                    log.info("task-{}, thread-name: {}", i, Thread.currentThread().getName());
-                    Thread.sleep(50);
-
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                log.info("task-{} complete", i);
-            });
-        }).get();
-        log.info("pool size: {}", forkJoinPool.getPoolSize());
-
-        Thread.sleep(60000);
-        log.info("=======================================");
-
-        forkJoinPool.submit(() -> {
-            IntStream.range(1, 5).parallel().forEach(i -> {
-                try {
-                    log.info("task2-{}, thread-name: {}", i, Thread.currentThread().getName());
-                    Thread.sleep(50);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                log.info("task2-{} complete", i);
-            });
-        }).get();
-        log.info("pool size: {}", forkJoinPool.getPoolSize());
-
-        log.info("end");
-    }
-
-    private static void testTtl() {
         TransmittableThreadLocal<String> ttl = new TransmittableThreadLocal<String>() {
             @Override
             protected void afterExecute() {
