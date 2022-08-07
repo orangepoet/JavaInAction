df63ca63fd7b25262191b2bf938a36b8273d81d8
orangepoet <chengzhi37@gmail.com>
2022-08-07 18:02:34 +0800

    nio

---
+++ inaction-netty/src/main/java/cn/orangepoet/inaction/netty/TimeClient.java
@@ -0,0 +1,66 @@
+package cn.orangepoet.inaction.netty;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class TimeClient {
+    public static void main(String[] args) throws InterruptedException {
+
+        TimeClient timeClient = new TimeClient();
+        timeClient.connect(8080, "localhost");
+    }
+
+    public void connect(int port, String host) throws InterruptedException {
+        NioEventLoopGroup group = new NioEventLoopGroup();
+        try {
+
+            Bootstrap b = new Bootstrap();
+            b.group(group)
+                .channel(NioSocketChannel.class)
+                .option(ChannelOption.TCP_NODELAY, true)
+                .handler(new ChannelInitializer<SocketChannel>() {
+
+                    @Override
+                    protected void initChannel(SocketChannel ch) throws Exception {
+                        ch.pipeline().addLast(new LineBasedFrameDecoder(1024));
+                        ch.pipeline().addLast(new StringDecoder());
+                        ch.pipeline().addLast(new TimeClientHandler());
+                    }
+                });
+
+            ChannelFuture channelFuture = b.connect(host, port);
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+
+    private class TimeClientHandler extends ChannelInboundHandlerAdapter {
+
+        private byte[] req = "QUERY TIME ORDER\n".getBytes();
+        private int counter = 0;
+
+        @Override
+        public void channelActive(ChannelHandlerContext ctx) throws Exception {
+            ByteBuf message;
+            for (int i = 0; i < 100; i++) {
+                message = Unpooled.buffer(req.length);
+                message.writeBytes(req);
+                ctx.writeAndFlush(message);
+            }
+        }
+    }
+}
