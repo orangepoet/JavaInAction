aa562313d607203385d256f6b38b9d9c5e43f205
orange.cheng <orange.cheng@nio.com>
2022-11-09 11:17:17 +0800

    upd

--- inaction-parallel/src/main/java/cn/orangepoet/inaction/parallel/executorservice/threadpool/App2.java
+++ inaction-parallel/src/main/java/cn/orangepoet/inaction/parallel/executorservice/threadpool/App2.java
@@ -4,17 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.SynchronousQueue;
-import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
@@ -23,12 +16,24 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class App2 {
     private static AtomicInteger seq = new AtomicInteger(0);
-    //    private static final ExecutorService executorService = Executors.newWorkStealingPool(4);
-    private static final ThreadPoolExecutor executorService = new ThreadPoolExecutor(4, 16, 10, TimeUnit.SECONDS, new ArrayBlockingQueue<>(1024), r -> {
-        Thread thread = new Thread(r);
-        thread.setName("thread" + seq.incrementAndGet());
-        return thread;
-    });
+
+    /**
+     * 当worker达到maxThreadPoolSize:16时，会加入阻塞队列
+     */
+//    private static final ThreadPoolExecutor THREAD_POOL_EXECUTOR = new ThreadPoolExecutor(4, 16, 10, TimeUnit.SECONDS, new ArrayBlockingQueue<>(1024), r -> {
+//        Thread thread = new Thread(r);
+//        thread.setName("thread" + seq.incrementAndGet());
+//        return thread;
+//    }, new ThreadPoolExecutor.AbortPolicy());
+
+    /**
+     * 当worker达到maxThreadPoolSize:16时，会拉起calling thread -> parallelStream.forkJoinPool.
+     */
+    private static final ThreadPoolExecutor THREAD_POOL_EXECUTOR = new ThreadPoolExecutor(4, 16, 10, TimeUnit.SECONDS, new SynchronousQueue<>(), r -> {
+            Thread thread = new Thread(r);
+            thread.setName("thread" + seq.incrementAndGet());
+            return thread;
+        }, new ThreadPoolExecutor.CallerRunsPolicy());
 
     public static void main(String[] args) {
         List<Supplier<Integer>> cals = new ArrayList<>();
@@ -38,7 +43,7 @@ public class App2 {
         }
 
         List<Integer> result = cals.parallelStream()
-                .map(supplier -> CompletableFuture.supplyAsync(supplier, executorService))
+                .map(supplier -> CompletableFuture.supplyAsync(supplier, THREAD_POOL_EXECUTOR))
                 .map(CompletableFuture::join)
                 .collect(Collectors.toList());
         log.info("result: {}", result);
