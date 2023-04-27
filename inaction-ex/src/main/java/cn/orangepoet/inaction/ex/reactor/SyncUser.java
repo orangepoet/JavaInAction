c919fc380af552f90a5f29ec49e831cb35de705f
orange.cheng <orange.cheng@nio.com>
2023-04-27 17:11:40 +0800

    upd

---
+++ inaction-ex/src/main/java/cn/orangepoet/inaction/ex/reactor/SyncUser.java
@@ -0,0 +1,150 @@
+package cn.orangepoet.inaction.ex.reactor;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.collections4.ListUtils;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+public class SyncUser {
+
+    public static final int BATCH_SIZE = 10_000;
+    public static final int USER_COUNT = 1_000;
+
+    /**
+     * * 像 service {getOpenIds, getUsers}, db {saveUsers, saveTags, saveEs}, publishEvents 都是阻塞的操作,
+     * * 即便使用了线程池, 在单个线程内操作上下游阻塞.
+     * *
+     * * 线程池: ForkJoinPool, 并发度: 8个, main+7*forkJoinPool.
+     */
+    public static void sync() {
+        int nextOpenid = 0;
+        int max = 5000;
+        for (int i = 0; i < 100000; i++) {
+            log.info("test2|seq->{}", i);
+            List<Integer> openIds = userIdList(nextOpenid); // 1500
+            if (openIds.isEmpty()) {
+                break;
+            }
+            ListUtils.partition(openIds, 100)
+                    .parallelStream()
+                    .forEach(partition -> {
+                        List<User> users = getUsers(partition);
+                        saveUsers(users);
+                        saveTags(users);
+                        saveEs(users);
+                        publishEvents(users);
+                    });
+            nextOpenid = openIds.get(openIds.size() - 1);
+        }
+    }
+
+
+    public static void flowSync() {
+        Scheduler userGetScheduler = Schedulers.newParallel("user-get", 2);
+        Scheduler userHandleScheduler = Schedulers.newParallel("user-handle", 4);
+
+        Flux.<List<Integer>, Integer>generate(() -> 0, (state, sink) -> {
+                    List<Integer> userIds = userIdList(state);
+                    if (CollectionUtils.isEmpty(userIds)) {
+                        sink.complete();
+                        return state;
+                    } else {
+                        sink.next(userIds);
+                        return userIds.get(userIds.size() - 1);
+                    }
+                })
+                .flatMap(userIdList ->
+                        Flux.fromStream(ListUtils.partition(userIdList, 100).stream()))
+                .flatMap(batch ->
+                        Flux.defer(() -> Mono.just(getUsers(batch)))
+                                .subscribeOn(userGetScheduler))
+                .flatMap(users ->
+                        Flux.defer(() -> {
+                                    saveUsers(users);
+                                    saveTags(users);
+                                    saveEs(users);
+                                    publishEvents(users);
+                                    return Mono.empty();
+                                })
+                                .subscribeOn(userHandleScheduler)
+                )
+                .blockFirst();
+
+        userGetScheduler.dispose();
+        userHandleScheduler.dispose();
+    }
+
+
+    private static List<Integer> userIdList(int nextid) {
+        log.info("userIdList|nextid->{}", nextid);
+
+        mockRun(1000);
+
+        if (nextid >= USER_COUNT) {
+            return Collections.emptyList();
+        }
+        List<Integer> result = new ArrayList<>();
+        int start = nextid + 1;
+        int end = Math.min(USER_COUNT, start + BATCH_SIZE);
+
+        for (int i = start; i <= end; i++) {
+            result.add(i);
+        }
+        log.info("userIdList|return {}~{}", result.get(0), result.get(result.size() - 1));
+        return result;
+    }
+
+    private static List<User> getUsers(List<Integer> openIds) {
+        log.info("getUsers|range|{}~{}", openIds.get(0), openIds.get(openIds.size() - 1));
+        List<User> users = openIds.stream().map(id -> new User(id)).collect(Collectors.toList());
+        mockRun(1000);
+        return users;
+    }
+
+    private static void saveUsers(List<User> users) {
+        log.info("saveUsers|range|{}~{}", users.get(0).getId(), users.get(users.size() - 1).getId());
+        mockRun(1000);
+    }
+
+    private static void saveTags(List<User> users) {
+        log.info("saveTags|range|{}~{}", users.get(0).getId(), users.get(users.size() - 1).getId());
+        mockRun(1000);
+    }
+
+    private static void saveEs(List<User> users) {
+        log.info("saveEs|range|{}~{}", users.get(0).getId(), users.get(users.size() - 1).getId());
+        mockRun(1000);
+    }
+
+    private static void publishEvents(List<User> users) {
+        log.info("publishEvents|range|{}~{}", users.get(0).getId(), users.get(users.size() - 1).getId());
+        mockRun(1000);
+    }
+
+    private static void mockRun(long ms) {
+        try {
+            Thread.sleep(ms);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Getter
+    @ToString
+    @AllArgsConstructor
+    private static class User {
+        private Integer id;
+    }
+}
--- inaction-ex/src/test/java/cn/orangepoet/inaction/ex/reactor/ReactorTests.java
+++ inaction-ex/src/test/java/cn/orangepoet/inaction/ex/reactor/ReactorTests.java
@@ -3,22 +3,17 @@ package cn.orangepoet.inaction.ex.reactor;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
+import org.reactivestreams.Subscription;
+import reactor.core.publisher.BaseSubscriber;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CountDownLatch;
 
 @Slf4j
 public class ReactorTests {
-    private static void mockRun(long ms) {
-        try {
-            Thread.sleep(ms);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
     /**
      * 限制发送速率
      *
@@ -46,20 +41,137 @@ public class ReactorTests {
         log.info("end");
     }
 
-    /**
-     * 通过flatMap并发
-     */
-    @Test
-    public void concurrencyTest() {
-        Flux.range(1, 100)
-                .doOnSubscribe(s -> s.request(8))
-                .flatMap(next -> process(next), 4)
-                .subscribe(x -> log.info("x->{}", x));
+    private static void mockRun(long ms) {
+        try {
+            Thread.sleep(ms);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
+    /**
+     * parallel Flux
+     */
+    @Test
+    public void FlapMap_Parallel() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(1);
+        Flux.range(1, 100)
+                .parallel()
+                .runOn(Schedulers.boundedElastic())
+                .map(next -> {
+                    log.info("flatMap({})", next);
+                    mockRun(1000);
+                    return next * 10;
+                })
+                .log()
+                .doOnComplete(latch::countDown)
+                .subscribe();
+        latch.await();
+    }
 
-    private Mono<Integer> process(int next) {
-        mockRun(1000);
-        return Mono.just(next * 10);
+    /**
+     * Mono的同步方法与异步方法
+     */
+    @Test
+    public void test1() {
+        CountDownLatch cdl = new CountDownLatch(2);
+        Mono.fromSupplier(() -> {
+                    mockRun(2000);
+                    return 1;
+                })
+                .doOnTerminate(cdl::countDown)
+                .log()
+                .subscribe();
+
+
+        Mono.fromFuture(CompletableFuture.supplyAsync(() -> {
+                    mockRun(2000);
+                    return 1;
+                }))
+                .doOnTerminate(cdl::countDown)
+                .log()
+                .subscribe();
+
+        try {
+            cdl.await();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * subscribe 拉取模式
+     */
+    @Test
+    public void SubscribePullRequest() {
+        Flux.<Integer>create(emitter -> {
+                    for (int i = 0; i < 100; i++) {
+                        log.info("emitter->{}", i);
+                        emitter.next(i);
+                    }
+                    emitter.complete();
+                })
+                .log()
+                .subscribe(new BaseSubscriber<Integer>() {
+                    private final int bufferSize = 8;
+                    private int count;
+
+                    @Override
+                    protected void hookOnSubscribe(Subscription subscription) {
+                        request(bufferSize);
+                    }
+
+                    @Override
+                    protected void hookOnNext(Integer value) {
+                        mockRun(1000);
+                        count++;
+//                         reached the maximum buffer size, request more items
+                        if (count == bufferSize) {
+                            count = 0;
+                            request(bufferSize);
+                        }
+                    }
+                });
+        log.info("end");
+    }
+
+    @Test
+    public void testSyncUsers() {
+        SyncUser.sync();
+    }
+
+    @Test
+    public void testFlowSyncUsers() throws InterruptedException {
+        SyncUser.flowSync();
+    }
+
+    @Test
+    public void testParallel() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(1);
+        Flux.range(1, 10)
+                .subscribeOn(Schedulers.parallel())
+                .map(i -> {
+                    mockRun(1000);
+                    return i * 10;
+                })
+                .doOnComplete(() -> {
+                    log.info("complete");
+                    latch.countDown();
+                })
+                .subscribe(next -> log.info("next->{}", next));
+        latch.await();
+    }
+
+    @Test
+    public void testBlock() {
+        Flux.range(1, 10)
+                .doOnNext(item -> System.out.println("Processing item " + item))
+                .blockFirst();
+
+
+        Flux.range(1, 10)
+                .doOnNext(item -> System.out.println("Processing item " + item))
+                .blockLast();
+
     }
 }
