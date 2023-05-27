16158ede129abde0deb993dd0315cedc97e014d9
orange.cheng <orange.cheng@nio.com>
2023-05-27 19:55:07 +0800

    fixup

--- inaction-algorithm/src/test/java/cn/orangepoet/inaction/algorithm/games/flightattack/FlightMapTest.groovy
+++ inaction-algorithm/src/test/java/cn/orangepoet/inaction/algorithm/games/flightattack/FlightMapTest.groovy
@@ -1,11 +1,6 @@
 package cn.orangepoet.inaction.algorithm.games.flightattack
 
-import cn.orangepoet.inaction.algorithm.games.flightattack.Flight
-import cn.orangepoet.inaction.algorithm.games.flightattack.FlightMap
-import cn.orangepoet.inaction.algorithm.games.flightattack.FlightUnit
-import cn.orangepoet.inaction.algorithm.games.flightattack.MatchResult
-import cn.orangepoet.inaction.algorithm.games.flightattack.Position
-import cn.orangepoet.inaction.algorithm.games.flightattack.Application
+
 import spock.lang.Specification
 
 /**
